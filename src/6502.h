90bb92a4bf4a04e7618bb623c100919b1150e3be
Louis <barneslouis225@gmail.com>
2023-09-23 13:10:12 +0100

    added instruction data

--- src/6502.h
+++ src/6502.h
@@ -1,5 +1,5 @@
-#ifndef 6502_H
-#define 6502_H
+// #ifndef 6502_H
+// #define 6502_H
 
 #include <stdint.h>
 
@@ -8,7 +8,7 @@
 typedef struct Status {
     uint8_t negative;
     uint8_t overflow;
-    uint8_t break;
+    uint8_t brk;
     uint8_t decimal;
     uint8_t interrupt;
     uint8_t zero;
@@ -29,6 +29,105 @@ typedef struct CPU {
     uint8_t memory[0xffff];
 } CPU;
 
+typedef enum {
+    /* load*/
+    LDA,
+    LDX,
+    LDY,
+    STA,
+    STX,
+    STY,
+
+    /* transfer */
+    TAX,
+    TAY,
+    TSX,
+    TXA,
+    TXS,
+    TYA,
+
+    /* stack */
+    PHA,
+    PHP,
+    PLA,
+    PLP,
+
+    /* shift */
+    ASL,
+    LSR,
+    ROL,
+    ROR,
+
+    /* logical */
+    AND,
+    BIT,
+    EOR,
+    ORA,
+
+    /* arithmetic */
+    ADC,
+    CMP,
+    CPX,
+    CPY,
+    SBC,
+
+    /* increment */
+    DEC,
+    DEX,
+    DEY,
+    INC,
+    INX,
+    INY,
+
+    /* control */
+    BRK,
+    JMP,
+    JSR,
+    RTI,
+    RTS,
+
+    /* branch */
+    BCC,
+    BCS,
+    BEQ,
+    BMI,
+    BNE,
+    BPL,
+    BVC,
+    BVS,
+
+    /* flags */
+    CLC,
+    CLD,
+    CLI,
+    CLV,
+    SEC,
+    SED,
+    SEI,
+
+    NOP,
+} Operation;
+
+typedef enum {
+    Implied,
+    Accumulator,
+    Immediate,
+    Absolute,
+    XAbsolute,
+    YAbsolute,
+    AbsoluteIndirect,
+    ZeroPage,
+    XZeroPage,
+    YZeroPage,
+    XZeroPageIndirect,
+    ZeroPageIndirectY,
+} AddressingMode;
+
+typedef struct Instruction {
+    Operation operation;
+    AddressingMode addressing_mode;
+} Instruction;
+
 /* functions */
 
-#endif /* 6502_H */
+// #endif /* 6502_H */
