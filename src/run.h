4e4925290da468dd66258fad2dd3bc3741bf356f
Louis <barneslouis225@gmail.com>
2023-09-29 11:06:53 +0100

    added outline for running functions

--- src/run.h
+++ src/run.h
@@ -1,4 +1,12 @@
 #ifndef RUN_H
 #define RUN_H
 
+#include "structure.h"
+
+void Init(CPU* cpu);
+void LoadROM(CPU* cpu, const char* filename);
+void Step(CPU* cpu, uint8_t print_debug);
+void Steps(CPU* cpu, uint32_t num_steps, uint32_t clock_speed, uint8_t print_debug);
+void Run(CPU* cpu, uint32_t clock_speed);
+
 #endif /* RUN_H */
